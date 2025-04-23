f338a147894edca29c0763ac1abaa2ea7c4404b3
RTookey <rachel.tookey@softwire.com>
2025-04-23 16:58:34 +0100

    Chore: added files to .gitignore

--- SupportBank/Utility/FileHandler.cs
+++ SupportBank/Utility/FileHandler.cs
@@ -4,6 +4,7 @@ namespace SupportBank.Utility;
 
 public static class FileHandler
 {
+    // add multiple csvs in 
     public const string Path = "Resources/DodgyTransactions2015.csv";
 
     private static readonly ILogger logger = LogManager.GetCurrentClassLogger();
@@ -29,23 +30,30 @@ public static class FileHandler
         using (StreamReader reader = new StreamReader(Path))
         {
             while (!reader.EndOfStream)
-            {   
+            {
                 lineNumber++;
                 string line = reader.ReadLine();
-                string[] values = line.Split(',');
-                Transaction newTransaction = new Transaction();
-                newTransaction.Date = ParseDate(values[0]);
-                newTransaction.From = values[1];
-                newTransaction.To = values[2];
-                newTransaction.Narrative = values[3];
-                if (Decimal.TryParse(values[4], out Decimal amount))
+                try
                 {
-                    newTransaction.Amount = amount;
-                    transactions.Add(newTransaction);
+                    string[] values = line.Split(',');
+                    Transaction newTransaction = new Transaction();
+                    newTransaction.Date = ParseDate(values[0]);
+                    newTransaction.From = values[1];
+                    newTransaction.To = values[2];
+                    newTransaction.Narrative = values[3];
+                    if (Decimal.TryParse(values[4], out Decimal amount))
+                    {
+                        newTransaction.Amount = amount;
+                        transactions.Add(newTransaction);
+                    }
+                    else
+                    {
+                        logger.Log(LogLevel.Error, "Could not parse transaction number: " + line);
+                    }
                 }
-                else
+                catch (Exception e)
                 {
-                    logger.Log(LogLevel.Error, "Could not parse transaction number: " + line);
+                    logger.Log(LogLevel.Error, $"Could not parse {line}: e.Message");
                 }
             }
         }
